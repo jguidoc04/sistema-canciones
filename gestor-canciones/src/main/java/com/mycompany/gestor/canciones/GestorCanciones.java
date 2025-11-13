799204579a5f246cc0693cb79e53416bc8c389ec
Jonathan Guido <jfguido97@gmail.com>
2025-11-12 21:53:28 -0600

    Eliminar Hola mundo

--- gestor-canciones/src/main/java/com/mycompany/gestor/canciones/GestorCanciones.java
+++ gestor-canciones/src/main/java/com/mycompany/gestor/canciones/GestorCanciones.java
@@ -13,10 +13,7 @@ public class GestorCanciones {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
         
-        System.out.println("Hola mundo");
-        
-        
-         System.out.println("Hola a todos");
+
         
     }
 }
