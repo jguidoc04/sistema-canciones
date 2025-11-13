d0bff34e1517ef70f9119c862c68cc86b8c569f8
Jonathan Guido <jfguido97@gmail.com>
2025-11-12 21:45:28 -0600

    Agregar otro Hola Mundo

--- gestor-canciones/src/main/java/com/mycompany/gestor/canciones/GestorCanciones.java
+++ gestor-canciones/src/main/java/com/mycompany/gestor/canciones/GestorCanciones.java
@@ -13,5 +13,7 @@ public class GestorCanciones {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
         
+        System.out.println("Hola mundo");
+        
     }
 }
